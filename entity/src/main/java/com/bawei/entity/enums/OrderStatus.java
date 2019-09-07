b2557c2155df94af909f5fdb4eb766804aee4cab
maojinyu <qdrain_mao@126.com>
2019-09-07 11:25:17 +0800

    git initial

---
+++ entity/src/main/java/com/bawei/entity/enums/OrderStatus.java
@@ -0,0 +1,17 @@
+package com.bawei.entity.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum OrderStatus {
+    INIT("初始化"),
+    PROCESS("处理中"),
+    SUCCESS("成功"),
+    FAIL("失败");
+
+    private String desc;
+
+    OrderStatus(String desc) {
+        this.desc = desc;
+    }
+}
